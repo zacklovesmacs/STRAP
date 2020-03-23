848c97160e603feffb096e31879e3104c62982f6
zacklovesmacs <50600805+zacklovesmacs@users.noreply.github.com>
2020-03-23 11:04:06 -0700

    analysis and generation of edited csv is successful. code may still need touchup but can operate

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 """
 Purpose:
-  Reads the given csv file and formats the contents and saves a 
+  Reads the given csv file and formats the contents and saves a
   copy for use within the excel spreadsheet.
 """
 
@@ -13,12 +13,36 @@ import csv
 
 
 def create_folder(name, return_path=False):
+    """ Creates folder with a given name and optional return of new path. """
     path = os.getcwd()
     folder = f"{path}\{name}"
     if not os.path.exists(folder):
         os.mkdir(folder)
     if return_path:
         return folder
+
+
+def current_time():
+    """ Returns the local time in ISO format. Without microseconds. """
+    return datetime.datetime.now().replace(microsecond=0).isoformat().replace(':', "'")
+
+
+def sort_by_rate(entries, increasing_order=False):
+    entries.sort(key=lambda x: x[len(x)-1],
+                 reverse=(increasing_order == False))
+    return entries
+
+
+def get_column_titles(file):
+    column_header = str(file.readline()).split(',')
+
+    # read first line (header) and store the indexes for "Employee Name", "Units", "UPH"
+    columns = {
+        'employee': column_header.index("Employee Name"),
+        'units': column_header.index("Units"),
+        'uph': column_header.index("UPH")
+    }
+    return columns
 
 
 if __name__ == '__main__':
@@ -33,17 +57,11 @@ if __name__ == '__main__':
             file_path = f"{path_original_files}\{file_name}"
 
             with open(file_path, 'r') as f:
-                column_header = str(f.readline()).split(',')
-
-                # read first line (header) and store the indexes for "Employee Name", "Units", "UPH"
-                columns = {
-                    'employee': column_header.index("Employee Name"),
-                    'units': column_header.index("Units"),
-                    'uph': column_header.index("UPH")
-                }
-
+                columns = get_column_titles(f)
                 csv_file = csv.reader(f)
+
                 data_entries = []
+
                 # loop through file line-by-line
                 for row in csv_file:
                     # if "UPH" != 0:
@@ -57,25 +75,16 @@ if __name__ == '__main__':
                         # calculate the stow rate (UPH/60) to one decimal
                         stow_rate = float('{:.1f}'.format(uph / 60))
 
-                        print(
-                            f"Employee: {employee}  Units: {units}  UPH: {uph}  Rate: {stow_rate}")
-
                         # append the stow rate to the data obtained
                         data_entries.append([employee, units, stow_rate])
 
-                # csv name shoud be date and time at generation
-                generated_file_name = "{}_stow rates".format(
-                    str(datetime.datetime.now()).replace(":", " "))
+            # csv name shoud be date and time at generation
+            generated_file_name = f"{current_time()}_stow rates"
+            edited_file_path = f"{path_formatted_files}\{generated_file_name}"
 
-                def Sort(y):
-                    y.sort(key=lambda x: x[len(x)-1], reverse=True)
-                    return y
-
-                # open new csv inside copy folder
-                with open(generated_file_name, 'w', newline='') as edited_csv:
-                    csv.writer(edited_csv).writerows(Sort(data_entries))
-            # append newly collected data to new csv
-
+            # save new csv inside copy folder
+            with open(edited_file_path, 'w', newline='') as edited_csv:
+                csv.writer(edited_csv).writerows(sort_by_rate(data_entries))
         pass
     except Exception as e:
         print(e)
