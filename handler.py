7d0bc3efae8751312a652385bcc6a5566a13a195
zacklovesmacs <50600805+zacklovesmacs@users.noreply.github.com>
2020-03-25 13:50:03 -0700

    Added console interaction and File save locations
    +++
    The console will now display and ask the user to choose a file if there are more than one files within "Original Files".
    
    +++
    Finalized excel files are now stored inside the 'Formatted Files' folder. They are also time stamped for future reference, if needed.

--- handler.py
+++ handler.py
@@ -18,7 +18,7 @@ class Analysis():
         self.csv_file = csv.reader(file)
         self.columns = self.get_headers(file) if has_headers else None
 
-    def analyze(self):
+    def analyze(self, sorted=True):
         ''' Performs analysis for each row and saves the output to a csv. '''
         for row in self.csv_file:
             self.update_row_data(row)
@@ -26,6 +26,9 @@ class Analysis():
             if self.employee_id not in Analysis.employees_completed:
                 Analysis.employees_completed.append(self.employee_id)
                 self.append_data_entry()
+
+            if sorted:
+                Analysis.sorted_by_rate(Analysis.data_entries)
 
     def append_data_entry(self):
         Analysis.data_entries.append(
@@ -64,9 +67,10 @@ class Analysis():
     @staticmethod
     def sorted_by_rate(entries, increasing_order=False):
         if not increasing_order:
-            entries.sort(key=lambda x: x[len(x)-1], reverse=(increasing_order == False))
+            entries.sort(key=lambda x: x[len(x)-1],
+                         reverse=(increasing_order == False))
         else:
-            entries.sort(key=lambda x: x[len(x)-1])   
+            entries.sort(key=lambda x: x[len(x)-1])
         return entries
 
     @staticmethod
@@ -78,27 +82,20 @@ class Analysis():
 
 
 class Excel():
-    ''' Uses pandas and openpyxl to handle excel doc customizations '''
-    @staticmethod
-    def csv_to_excel(file: str):
-        if not file:
-            raise "No file was given to convert"
-        csv_file = pandas.read_csv(file)
+    ''' Uses openpyxl to handle excel doc customizations '''
 
-        with pandas.ExcelWriter('copy.xlsx', mode='a') as writer:
-            csv_file.to_excel(writer, index=False)
-    
     @staticmethod
-    def append_data_to_sheet(data):
-        template_file = 'copy.xlsx'
+    def append_data_to_template(data, save_as=None):
+        template_file = os.getcwd() + "\\Excel Template\\stow rate template.xlsx"
         wb = openpyxl.load_workbook(template_file)
         ws = wb.active
+
+        file_name = 'sample' if save_as is None else save_as
 
         for row, info in enumerate(data, start=1):
             print(row, info)
             ws['A{}'.format(row)] = info[0]
             ws['B{}'.format(row)] = info[1]
             ws['C{}'.format(row)] = info[2]
-        
-        wb.save('sample.xlsx')
-        print('hsdhfh')
+
+        wb.save(file_name + '.xlsx')
--- main.py
+++ main.py
@@ -6,17 +6,19 @@ Purpose:
   copy for use within the excel spreadsheet.
 """
 
+
 import os
 import time
 import datetime
 
+import glob
 from handler import Excel, Analysis
 
 
 def create_folder(name):
     """ Creates folder with a given name and optional return of new path. """
     if not os.path.exists(name):
-        os.makekdirs(name)
+        os.mkdir(name)
 
 
 def create_path(path, file):
@@ -39,13 +41,44 @@ if __name__ == '__main__':
     create_folder(path_formatted_files)
 
     original_files = os.listdir(path_original_files)
-    
+
+    def clear_console(): return os.system('cls')
+
     if not original_files:
         print("There is are no files to process in 'Original Files'")
         input("Press return to exit...")
         exit()
 
-    for file_name in os.listdir(path_original_files):
+    chosen_file = original_files[0]
+
+    if len(original_files) > 1:
+        chosen_file = None
+
+        while chosen_file is None:
+            clear_console()
+            print("There are more than 1 file inside the folder!")
+            print("\nPlease choose one of the following:")
+
+            for number, f in enumerate(os.listdir(path_original_files)):
+                print("\t[{}] {}".format(number, f))
+            x = int(input("\nEnter a number: "))
+
+            if x not in range(len(original_files)):
+                clear_console()
+                print("Invalid Selection!")
+                time.sleep(1)
+                continue
+
+            chosen_file = original_files[x]
+            print(chosen_file)
+
+        clear_console()
+
+    for count, file_name in enumerate(os.listdir(path_original_files)):
+
+        if file_name != chosen_file:
+            continue
+
         # open file inside the folder
         file_path = create_path(path_original_files, file_name)
 
@@ -58,16 +91,19 @@ if __name__ == '__main__':
                 generated_file_name = "{}_stow rates".format(current_time())
                 edited_file_path = create_path(
                     path_formatted_files, generated_file_name)
-                csv_file.save(edited_file_path)
 
-            # convert csv to excel
-            Excel.csv_to_excel(edited_file_path)
-            Excel.append_data_to_sheet(Analysis.data_entries)
+            Excel.append_data_to_template(Analysis.data_entries, save_as=edited_file_path)
+            break
+
         except FileNotFoundError() as fnf:
             print("File not found:\n", fnf)
         except Exception() as e:
             print(e)
 
-    #os.remove(path_original_files)
+    # os.remove(path_original_files)
     #print("Conversion completed!")
-    time.sleep(1.5)
+    print("\nConversion Complete!")
+    print("Opening Excel Document...")
+    time.sleep(2)
+
+    os.startfile(edited_file_path + '.xlsx')
